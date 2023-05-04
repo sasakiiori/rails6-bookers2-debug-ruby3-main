51ff8baff776ed0af5b7aa2461a1e9145b195dce
sasakiiori <126736985+sasakiiori@users.noreply.github.com>
2023-05-04 10:43:36 +0000

    initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,13 @@
 class BooksController < ApplicationController
-
+before_action :ensure_correct_user, only: [:update, :edit]
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -32,15 +34,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+
+    def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+  end
+end
 end
